1905855fc6e7ed3e006c201d6ed72aa341b2b2be
GonzaIba <gonzaloiba.28.00@gmail.com>
2025-09-21 22:47:08 -0300

    feat: IDOr notification and add database updated

--- TFI_TP_FINAL_FORUMS/01.Web/ApiForums/Mapping/Mapping.cs
+++ TFI_TP_FINAL_FORUMS/01.Web/ApiForums/Mapping/Mapping.cs
@@ -155,7 +155,6 @@ namespace ApiForums.Mapping
             CreateMap<NotificacionesModel, NotificationsResponse>()
                 .ForMember(dest => dest.Message, opt => opt.MapFrom(src => src.Mensaje))
                 .ForMember(dest => dest.Readed, opt => opt.MapFrom(src => src.Leida))
-                .ForMember(dest => dest.CodeUser, opt => opt.MapFrom(src => src.IDUsuario))
                 .ForMember(dest => dest.Date, opt => opt.MapFrom(src => src.FechaNotificacion))
                 .ForMember(dest => dest.CodeNotification, opt => opt.MapFrom(src => src.IDNotificacion));
 
--- TFI_TP_FINAL_FORUMS/02.Core/Core.Domain/Response/NotificationsResponse.cs
+++ TFI_TP_FINAL_FORUMS/02.Core/Core.Domain/Response/NotificationsResponse.cs
@@ -9,7 +9,6 @@ namespace Core.Domain.Response
     public class NotificationsResponse
     {
         public int CodeNotification { get; set; }
-        public string CodeUser { get; set; }
         public string Message { get; set; }
         public DateTime Date { get; set; }
         public bool Readed { get; set; }
